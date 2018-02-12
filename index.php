a49fc946f519389bb5dfeb944323be5a3b6e4483
skyriser92 <35499411+skyriser92@users.noreply.github.com>
2018-02-12 15:14:54 -0500

    first commit
    changed date and added date comparision

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-$date =  date('Y-m-d', time());
+$date =  date('Y/m/d', time());
 echo "The value of \$date: ".$date."<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
+
+if ($date - $tar > 0 ) {
+	echo "the future";
+}
+elseif ($date - $tar < 0) {
+	echo "the past";
+}
+else {
+	echo "oops";
+}
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
